278ec3501f6f352b3525951b8fba890384641dcf
satishgupta07 <satishgupta.cse01@gmail.com>
2020-07-27 00:17:05 +0530

    Saving or Posting data in Mongo Database
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+const { json } = require('express')
+const User = mongoose.model("User")
 
 router.get('/',(req,res)=>{
     res.send("Hello")
@@ -10,7 +13,28 @@ router.post('/signup',(req,res)=>{
     if(!email || !password || !name){
        return res.status(422).json({error:"please add all the fields"})
     }
-    res.json({message:"successfully posted"})
+    User.findOne({email:email})
+    .then((savedUser)=>{
+        if(savedUser){
+            return res.status(422).json({error:"user already exists with that email"})
+        }
+        const user = new User({
+            email,
+            password,
+            name
+        })
+
+        user.save()
+        .then(user=>{
+            res.json({message:"saved successfully"})
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    })
+    .catch(err => {
+        console.log(err)
+    })
 })
 
 module.exports = router
