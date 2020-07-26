0f77c116724f6e2815cbb8a0da2048228082bc83
satishgupta07 <satishgupta.cse01@gmail.com>
2020-07-26 18:12:18 +0530

    middleware
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- server/app.js
+++ server/app.js
@@ -2,8 +2,21 @@ const express = require('express')
 const app = express()
 const PORT = 5000
 
+const customMiddleware = (req, res, next) => {
+    console.log("Middleware executed!")
+    next()
+}
+
+// app.use(customMiddleware)
+
 app.get('/',(req,res)=>{
+    console.log("home")
     res.send("Hello World!")
+})
+
+app.get('/about',customMiddleware,(req,res)=>{
+    console.log("about")
+    res.send("About Page!")
 })
 
 app.listen(PORT,()=>{
