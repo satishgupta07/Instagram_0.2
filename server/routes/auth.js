a736d4c985cddd81fef421348c00a9aaac3eb2ed
satishgupta07 <satishgupta.cse01@gmail.com>
2020-07-27 00:35:54 +0530

    Hashed the password using bcrypt js
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,10 +3,7 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const { json } = require('express')
 const User = mongoose.model("User")
-
-router.get('/',(req,res)=>{
-    res.send("Hello")
-})
+const bcrypt = require('bcryptjs')
 
 router.post('/signup',(req,res)=>{
     const {name,email,password} = req.body
@@ -18,19 +15,23 @@ router.post('/signup',(req,res)=>{
         if(savedUser){
             return res.status(422).json({error:"user already exists with that email"})
         }
-        const user = new User({
-            email,
-            password,
-            name
+        bcrypt.hash(password,12)
+        .then(hashedpassword=>{
+            const user = new User({
+                email,
+                password:hashedpassword,
+                name
+            })
+    
+            user.save()
+            .then(user=>{
+                res.json({message:"saved successfully"})
+            })
+            .catch(err=>{
+                console.log(err)
+            })
         })
-
-        user.save()
-        .then(user=>{
-            res.json({message:"saved successfully"})
-        })
-        .catch(err=>{
-            console.log(err)
-        })
+       
     })
     .catch(err => {
         console.log(err)
