298069fe045785274c3cc8e077590b29d037beff
satishgupta07 <satishgupta.cse01@gmail.com>
2020-07-27 00:50:53 +0530

    created signin route
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -38,4 +38,29 @@ router.post('/signup',(req,res)=>{
     })
 })
 
+router.post('/signin', (req,res)=>{
+    const {email,password} = req.body
+    if(!email || !password){
+        res.status(422).json({error:"please add email or password"})
+    }
+    User.findOne({email:email})
+    .then(savedUser=>{
+        if(!savedUser){
+           return res.status(422).json({error:"Invalid Email or password"})
+        }
+        bcrypt.compare(password,savedUser.password)
+        .then(doMatch=>{
+            if(doMatch){
+                res.json({message:"sucessfully signed in"})
+            }
+            else{
+                return res.status(422).json({error:"Invalid Email or password"})
+            }
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    })
+})
+
 module.exports = router
