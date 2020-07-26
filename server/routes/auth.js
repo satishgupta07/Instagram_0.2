e0600f19a086f41d6355bb175b58f579a5b9d852
satishgupta07 <satishgupta.cse01@gmail.com>
2020-07-27 00:05:22 +0530

    getting data in request body
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

---
+++ server/routes/auth.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/',(req,res)=>{
+    res.send("Hello")
+})
+
+router.post('/signup',(req,res)=>{
+    const {name,email,password} = req.body
+    if(!email || !password || !name){
+       return res.status(422).json({error:"please add all the fields"})
+    }
+    res.json({message:"successfully posted"})
+})
+
+module.exports = router
