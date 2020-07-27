95006230f22c5a955925c9ce27d060d328603563
satishgupta07 <satishgupta.cse01@gmail.com>
2020-07-27 10:24:43 +0530

    Post schema and post route
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- server/app.js
+++ server/app.js
@@ -4,10 +4,6 @@ const mongoose = require('mongoose')
 const PORT = 5000
 const {MONGOURI} = require('./keys')
 
-require('./models/user')
-
-app.use(express.json())
-app.use(require('./routes/auth'))
 
 mongoose.connect(MONGOURI,{
       useNewUrlParser:true,
@@ -21,6 +17,13 @@ mongoose.connection.on('connected', () => {
 mongoose.connection.on('error', (err) => {
     console.log("err connecting",err)
 })
+
+require('./models/user')
+require('./models/post')
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+app.use(require('./routes/post'))
 
 app.listen(PORT,()=>{
     console.log("Server is Running on",PORT)
