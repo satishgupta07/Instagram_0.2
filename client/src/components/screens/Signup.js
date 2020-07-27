928085d8f997e74c247afaf2816eb03a23a4d6be
satishgupta07 <satishgupta.cse01@gmail.com>
2020-07-28 00:29:21 +0530

    Working on User Signup by posting data
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,7 +1,39 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React,{useState} from 'react';
+import {Link,useHistory} from 'react-router-dom'
+import M from 'materialize-css'
 
 const Signup = () => {
+   const history = useHistory()
+   const [name,setName] = useState("")
+   const [password,setPassword] = useState("")
+   const [email,setEmail] = useState("")
+   const PostData = () =>{
+      if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
+         M.toast({html: "Invalid email", classes:"#c62828 red darken-3"})
+         return
+      }
+      fetch("/signup",{
+         method:"post",
+         headers:{
+            "Content-Type":"application/json"
+         },
+         body:JSON.stringify({
+            name,
+            password,
+            email
+         })
+      }).then(res=>res.json())
+      .then(data=>{
+         if(data.error){
+            M.toast({html: data.error, classes:"#c62828 red darken-3"})
+         }
+         else{
+            M.toast({html:data.message, classes:"#43a047 green darken-1"})
+            history.push('/signin')
+         }
+      })
+   }
+
     return (
         <div className="mycard">
              <div className="card-panel auth-card input-field">
@@ -9,17 +41,25 @@ const Signup = () => {
                 <input
                    type="text"
                    placeholder="name"
+                   value={name}
+                   onChange={(e)=>setName(e.target.value)}
                 />
                 <input
                    type="text"
                    placeholder="email"
+                   value={email}
+                   onChange={(e)=>setEmail(e.target.value)}
                 />
                  <input
                    type="text"
                    placeholder="password"
+                   value={password}
+                   onChange={(e)=>setPassword(e.target.value)}
                 />
                 
-              <button className="btn waves-effect waves-light #64b5f6 blue darken-1">Signup</button>
+              <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
+                      onClick={()=>PostData()}
+              >Signup</button>
 
               <h6>
                  <Link to="/signin">Already have an account ?</Link>
