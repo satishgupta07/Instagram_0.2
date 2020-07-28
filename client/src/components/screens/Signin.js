8f2ae300188771d4df0cd8c5e080a0f4e553d346
satishgupta07 <satishgupta.cse01@gmail.com>
2020-07-28 09:16:12 +0530

    Working on User Signin
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- client/src/components/screens/Signin.js
+++ client/src/components/screens/Signin.js
@@ -1,7 +1,39 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React,{useState} from 'react';
+import {Link,useHistory} from 'react-router-dom'
+import M from 'materialize-css'
 
 const Signin = () => {
+   const history = useHistory()
+   const [password,setPassword] = useState("")
+   const [email,setEmail] = useState("")
+   const PostData = () =>{
+      if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
+         M.toast({html: "Invalid email", classes:"#c62828 red darken-3"})
+         return
+      }
+      fetch("/signin",{
+         method:"post",
+         headers:{
+            "Content-Type":"application/json"
+         },
+         body:JSON.stringify({
+            password,
+            email
+         })
+      }).then(res=>res.json())
+      .then(data=>{
+         console.log(data)
+         if(data.error){
+            M.toast({html: data.error, classes:"#c62828 red darken-3"})
+         }
+         else{
+            M.toast({html:"Signedin Success", classes:"#43a047 green darken-1"})
+            history.push('/')
+         }
+      }).catch(err=>{
+         console.log(err)
+      })
+   }
     return (
         <div className="mycard">
              <div className="card-panel auth-card input-field">
@@ -9,13 +41,19 @@ const Signin = () => {
                 <input
                    type="text"
                    placeholder="email"
+                   value={email}
+                   onChange={(e)=>setEmail(e.target.value)}
                 />
                  <input
                    type="text"
                    placeholder="password"
+                   value={password}
+                   onChange={(e)=>setPassword(e.target.value)}
                 />
                 
-              <button className="btn waves-effect waves-light #64b5f6 blue darken-1">Login</button>
+              <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
+                      onClick={()=>PostData()}
+                      >Login</button>
 
               <h6>
                  <Link to="/signup">Don't have an account ?</Link>
