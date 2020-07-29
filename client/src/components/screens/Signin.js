76b120ce44d77dc18985f6c05b4d0fbfed500b84
satishgupta07 <satishgupta.cse01@gmail.com>
2020-07-29 10:27:45 +0530

    Implementing Logout
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- client/src/components/screens/Signin.js
+++ client/src/components/screens/Signin.js
@@ -50,7 +50,7 @@ const Signin = () => {
                    onChange={(e)=>setEmail(e.target.value)}
                 />
                  <input
-                   type="text"
+                   type="password"
                    placeholder="password"
                    value={password}
                    onChange={(e)=>setPassword(e.target.value)}
