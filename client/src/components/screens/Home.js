fa8ff82c2cd5a13e97d022a01111da462f8b602b
satishgupta07 <satishgupta.cse01@gmail.com>
2020-07-30 20:38:19 +0530

    Implementing Comment in ReactJS
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -67,6 +67,33 @@ const Home = () => {
         })
     }
 
+    const makeComment = (text,postId) => {
+        fetch('/comment',{
+            method:"put",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId,
+                text
+            })
+        }).then(res=>res.json())
+        .then(result=>{
+            console.log(result)
+            const newData = data.map(item => {
+                if(item._id == result._id){
+                    return result
+                }else{
+                    return item
+                }
+            })
+            setData(newData)
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+
     return (
         <div className="home">
         {
@@ -93,10 +120,21 @@ const Home = () => {
                   <h6>{item.likes.length} likes</h6>
                   <h6>{item.title}</h6>
                   <p>{item.body}</p>
-                  <div style={{display: "flex"}} >
-                      <input type="text" placeholder="add a comment"/>
-                      <button className="btn-floating btn waves-effect waves-light blue"><i className="material-icons">send</i></button>
-                  </div>
+                  {
+                      item.comments.map(record=>{
+                          return(
+                              <h6 key={record._id}><span style={{fontWeight:"500"}}>{record.postedBy.name}</span> {record.text}</h6>
+                          )
+                      })
+                  }
+                  <form onSubmit={(e)=>{
+                      e.preventDefault()
+                      makeComment(e.target[0].value,item._id)
+                  }}>
+                     
+                    <input type="text" placeholder="add a comment"/>
+                  </form>
+                  
                </div>
            </div>
 
