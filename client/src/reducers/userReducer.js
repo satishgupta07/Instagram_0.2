0b22cd1df8138e0272ac23f7933e4159a6954a0e
satishgupta07 <satishgupta.cse01@gmail.com>
2020-07-31 22:29:04 +0530

    Implementing Follow User in React on Client Side
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -7,5 +7,12 @@ export const reducer = (state,action)=>{
     if(action.type==="CLEAR"){
         return null
     }
+    if(action.type=="Update"){
+        return {
+            ...state,
+            follower: action.payload.followers,
+            following:action.payload.following
+        }
+    }
     return state
 }
