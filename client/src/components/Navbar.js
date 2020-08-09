4b4b08b34cde8e7aa5482ca39616aaf6521b0315
satishgupta07 <satishgupta.cse01@gmail.com>
2020-08-09 19:02:15 +0530

    Search User Feature Client-Side
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,24 @@
-import React,{useContext} from 'react'
+import React,{useContext,useRef,useEffect,useState} from 'react'
 import {Link,useHistory} from 'react-router-dom'
 import {UserContext} from '../App'
+import M from 'materialize-css'
 
 const Navbar = () => {
+    const searchModal = useRef(null)
+    const [search, setSearch] = useState("")
     const {state,dispatch} = useContext(UserContext)
     const history = useHistory()
+    useEffect(()=>{
+        M.Modal.init(searchModal.current)
+    },[])
     const renderList = () =>{
       if(state){
         return [
-          <li><Link to="/profile">Profile</Link></li>,
-            <li><Link to="/create">Create Post</Link></li>,
-            <li><Link to="/myfollowingspost">My Following Posts</Link></li>,
-            <li>
+          <li key="1"><i data-target="modal1" className="large material-icons modal-trigger" style={{color:"black"}}>search</i></li>,
+          <li key="2"><Link to="/profile">Profile</Link></li>,
+            <li key="3"><Link to="/create">Create Post</Link></li>,
+            <li key="4"><Link to="/myfollowingspost">My Following Posts</Link></li>,
+            <li key="5">
               <button className="btn waves-effect waves-light #c62828 red darken-3"
                       onClick={()=>{
                           localStorage.clear()
@@ -24,8 +31,8 @@ const Navbar = () => {
         ]
       }else{
         return [
-          <li><Link to="/signin">Login</Link></li>,
-          <li><Link to="/signup">Signup</Link></li>
+          <li key="6"><Link to="/signin">Login</Link></li>,
+          <li key="7"><Link to="/signup">Signup</Link></li>
         ]
       }
     }
@@ -38,6 +45,32 @@ const Navbar = () => {
                {renderList()}
           </ul>
         </div>
+
+        <div id="modal1" class="modal" ref={searchModal} style={{color:"black"}}>
+             <div className="modal-content">
+                <input 
+                  type="text"
+                  placeholder="search users"
+                  value={search}
+                  onChange={(e)=>setSearch(e.target.value)}
+                />
+                
+               <ul className="collection">
+                  <li className="collection-item">Alvin</li>
+                  <li className="collection-item">Alvin</li>
+                  <li className="collection-item">Alvin</li>
+                  <li className="collection-item">Alvin</li>
+                  <li className="collection-item">Alvin</li>
+                  <li className="collection-item">Alvin</li>
+                  <li className="collection-item">Alvin</li>
+                  <li className="collection-item">Alvin</li>
+               </ul>
+             </div>
+            <div className="modal-footer">
+               <button className="modal-close waves-effect waves-green btn-flat">Agree</button>
+            </div>
+        </div>
+
       </nav>
     )
 }
